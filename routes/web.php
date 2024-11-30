58c0d57cfb999522482ca4a9bf3ff8ea0ecf4e63
lialia.sakhno <lintume@gmail.com>
2024-11-30 13:34:00 +0200

    Add configs for package, add API key to .env.example, add app layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\WeatherSearch;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/search', WeatherSearch::class);
